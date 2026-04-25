3c12e791c54258e9dd4ffd399d3a3034853664d2
nmiyabe <nmiyabe_y@yahoo.co.jp>
2026-04-25 18:12:14 +0900

    front(React)ベース作成

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>React App</h1>
+      <p>Base application built with Vite, TypeScript, and Vanilla CSS.</p>
+    </div>
+  )
+}
+
+export default App
